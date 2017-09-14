bfaa39d57f010b3cd6b9a781ad8b9a74f680f094
nikitawani07 <wani.nikita07@gmail.com>
2017-09-15 03:24:30 +0530

    Add files via upload

---
+++ wikiEngine/src/Fields.java
@@ -0,0 +1,19 @@
+package wikiEngine;
+
+public class Fields {
+	int title;
+	int text;
+	int infobox;
+	int category;
+	int extlink;
+	int reference;
+	
+	Fields(){
+		this.title=0;
+		this.text=0;
+		this.infobox=0;
+		this.category=0;
+		this.extlink=0;
+		this.reference=0;
+	}
+}
